9c21e8d72a9b72bdfae84867f21629ceadc9fa67
m_fenu <michele.fenu@spindox.it>
2024-06-07 15:35:52 +0200

    Add Working Animation

--- Assets/MyScripts/BusinessLogic/Hotspot/WorkingHotspot.cs
+++ Assets/MyScripts/BusinessLogic/Hotspot/WorkingHotspot.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 namespace SH.BusinessLogic {
     public class WorkingHotspot : Hotspot
     {
-
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+            if(collision.collider.name == "EnemyController")
+            {
+                Debug.Log("enemy collided");
+            }
+        }
     }
 }
